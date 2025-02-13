1691d792f10eb0d2989c76260c45cc5a995b4193
Anik Shakya <aniklinkin@gmail.com>
2025-02-13 13:00:37 +0545

    [Fixed:: user controller] - Fixed Null Issue

--- lib/src/controllers/user_controller.dart
+++ lib/src/controllers/user_controller.dart
@@ -5,7 +5,7 @@ class UserController extends GetxController {
   final RxBool isLoginLoading = false.obs;
   int userPoints = 5000;
   String userImage = "https://images.squarespace-cdn.com/content/v1/56c346b607eaa09d9189a870/1551408857522-4ZFG11B2M7UPFYBFBRO0/FLAUNT-MAGAZINE-JOJI-2.jpg";
-  String userName = read("user")["name"] ?? "";
+  String userName = read("user") != "" ? read("user")["name"] : "No Data Found";
   String userId = read("user")["id"].toString();
 
   void startLoginLoading() {
