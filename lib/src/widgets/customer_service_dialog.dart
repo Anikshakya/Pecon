48c084f372f21807ccb5d0b41e02fdf4cfef6b91
Anik Shakya <aniklinkin@gmail.com>
2025-02-13 15:32:21 +0545

    [Fixed] - launch/intent to external app

--- lib/src/widgets/customer_service_dialog.dart
+++ lib/src/widgets/customer_service_dialog.dart
@@ -1,10 +1,8 @@
-
-import 'dart:io' show Platform;
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
+import 'package:pecon/src/app_config/launch_url.dart';
 import 'package:pecon/src/app_config/styles.dart';
-import 'package:url_launcher/url_launcher.dart';
 
 customerServiceDialog() {
   return Get.defaultDialog(
@@ -32,7 +30,7 @@ customerServiceDialog() {
                 ),
                 SizedBox(height: 15.h,),
                 InkWell(
-                  onTap: makePhoneCall,
+                  onTap: ()=> AppUtils().openLinkWithUrl("tel:1234567890"),
                   borderRadius: BorderRadius.circular(30.r),
                   child: Padding(
                     padding: EdgeInsets.symmetric(horizontal: 20.w, vertical: 10.h),
@@ -47,7 +45,7 @@ customerServiceDialog() {
                 ),
                 SizedBox(height: 5.h,),
                 InkWell(
-                  onTap: openWhatsApp,
+                  onTap: ()=> AppUtils().openLinkWithUrl("https://wa.me/1234567890"),
                   borderRadius: BorderRadius.circular(30.r),
                   child: Padding(
                     padding: EdgeInsets.symmetric(horizontal: 20.w, vertical: 10.h),
@@ -68,44 +66,4 @@ customerServiceDialog() {
       },
     ),
   );
-}
-
-//phone
-makePhoneCall() async {
-  final Uri callUri = Uri.parse("tel:1234567890");
-  debugPrint("Attempting to launch: $callUri");
-  if (await canLaunchUrl(callUri)) {
-    await launchUrl(callUri);
-  } else {
-    debugPrint("Could not launch $callUri");
-  }
-}
-
-//whatsapp
-openWhatsApp() async {
-  final Uri whatsappUri = Uri.parse("https://wa.me/1234567890");
-  debugPrint("Attempting to launch: $whatsappUri");
-
-  if (await canLaunchUrl(whatsappUri)) {
-    await launchUrl(whatsappUri);
-  } else {
-    // WhatsApp is not installed, redirect to download page
-    debugPrint("WhatsApp is not installed. Redirecting to download page.");
-
-    final Uri storeUri = Platform.isAndroid
-        ? Uri.parse("https://play.google.com/store/apps/details?id=com.whatsapp") // Android
-        : Uri.parse("https://apps.apple.com/app/whatsapp-messenger/id310633997"); // iOS
-
-    if (await canLaunchUrl(storeUri)) {
-      await launchUrl(storeUri);
-    } else {
-      // If the store cannot be opened, show a message
-      Get.snackbar(
-        "WhatsApp Not Installed",
-        "Please install WhatsApp to use this feature.",
-        snackPosition: SnackPosition.BOTTOM,
-        duration: const Duration(seconds: 3),
-      );
-    }
-  }
 }
