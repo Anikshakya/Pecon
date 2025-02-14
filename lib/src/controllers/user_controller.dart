5c27fb5e5c867b361c20aed47fb735bd33527119
Anik Shakya <aniklinkin@gmail.com>
2025-02-14 11:13:16 +0545

    [Managed] - null issue for number

--- lib/src/controllers/user_controller.dart
+++ lib/src/controllers/user_controller.dart
@@ -7,7 +7,7 @@ class UserController extends GetxController {
   String userImage = read("user") != ""? read("user")["profile_url"] ?? "https://images.squarespace-cdn.com/content/v1/56c346b607eaa09d9189a870/1551408857522-4ZFG11B2M7UPFYBFBRO0/FLAUNT-MAGAZINE-JOJI-2.jpg" : "https://images.squarespace-cdn.com/content/v1/56c346b607eaa09d9189a870/1551408857522-4ZFG11B2M7UPFYBFBRO0/FLAUNT-MAGAZINE-JOJI-2.jpg";
   String userName = read("user") != "" ? read("user")["name"] ?? "N/A" : "N/A";
   String userId =  read("user") != "" ? read("user")["id"].toString() != "null" ? read("user")["id"].toString() : "N/A" : "N/A";
-  String userNumber = read("user") != "" ? read("user")["number"] ?? 0 : 0;
+  String userNumber = read("user") != "" ? read("user")["number"] != "null" ? read("user")["number"].toString() : "N/A" : "N/A";
 
   void startLoginLoading() {
     isLoginLoading.value = true;
