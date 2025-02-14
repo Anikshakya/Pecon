aad470784e87146ddbf95fa07d6c0ba9c0950229
Anik Shakya <aniklinkin@gmail.com>
2025-02-14 11:06:10 +0545

    [Managed] - Added Null check for user data

--- lib/src/controllers/user_controller.dart
+++ lib/src/controllers/user_controller.dart
@@ -3,11 +3,11 @@ import 'package:pecon/src/app_config/read_write.dart';
 
 class UserController extends GetxController {
   final RxBool isLoginLoading = false.obs;
-  int userPoints = 5000;
-  String userImage = "https://images.squarespace-cdn.com/content/v1/56c346b607eaa09d9189a870/1551408857522-4ZFG11B2M7UPFYBFBRO0/FLAUNT-MAGAZINE-JOJI-2.jpg";
-  String userName = read("user") != "" ? read("user")["name"] : "No Data Found";
-  String userId = read("user")["id"].toString();
-  String userNumber = "+977 9576543457";
+  int userPoints = read("user") != "" ? read("user")["earned_points"] : 0;
+  String userImage = read("user") != ""? read("user")["profile_url"] ?? "https://images.squarespace-cdn.com/content/v1/56c346b607eaa09d9189a870/1551408857522-4ZFG11B2M7UPFYBFBRO0/FLAUNT-MAGAZINE-JOJI-2.jpg" : "https://images.squarespace-cdn.com/content/v1/56c346b607eaa09d9189a870/1551408857522-4ZFG11B2M7UPFYBFBRO0/FLAUNT-MAGAZINE-JOJI-2.jpg";
+  String userName = read("user") != "" ? read("user")["name"] : "N/A";
+  String userId =  read("user") != "" ? read("user")["id"] ?? "N/A" : "N/A";
+  String userNumber = read("user") != "" ? read("user")["number"] : 0;
 
   void startLoginLoading() {
     isLoginLoading.value = true;
