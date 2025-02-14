7cceb72ef938a4397a89933e9768dc5dc6ba9245
Anik Shakya <aniklinkin@gmail.com>
2025-02-14 11:17:20 +0545

    [Modifeid:: product Details]

--- lib/src/view/product_details.dart
+++ lib/src/view/product_details.dart
@@ -3,6 +3,7 @@ import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 import 'package:pecon/src/app_config/styles.dart';
+import 'package:pecon/src/widgets/custom_appbar.dart';
 import 'package:pecon/src/widgets/view_full_screen_image.dart';
 
 class ProductDetailsPage extends StatefulWidget {
@@ -26,125 +27,159 @@ class _ProductDetailsPageState extends State<ProductDetailsPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
+      appBar: appbar(),
+      body: SingleChildScrollView(
+        physics: const BouncingScrollPhysics(),
+        child: Column(
+          children: [
+            // Product Image Slider
+            productImageSlider(),
+            SizedBox(height: 20.h),
+            // Product Details
+            productDetails(),
+            SizedBox(height: 20.h),
+          ],
+        ),
+      ),
+    );
+  }
+
+  // About Product
+  productDetails() {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 20.sp),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          // Image Slider
-          CarouselSlider(
-            items: imageUrls.map((url) {
-              return GestureDetector(
-                onTap: (){
-                  Get.to(()=> FullScreenImagePage(imageUrl: url));
-                },
-                child: Container(
-                  color: const Color.fromARGB(255, 236, 236, 236),
-                  child: Image.network(
-                    url,
-                    fit: BoxFit.cover,
-                    width: double.infinity,
-                  ),
-                ),
-              );
-            }).toList(),
-            options: CarouselOptions(
-              height: 300,
-              viewportFraction: 1.0,
-              autoPlay: true,
-              onPageChanged: (index, reason) {
-                setState(() {
-                  activeIndex = index;
-                });
-              },
-            ),
+          Text(
+            "Category 2",
+            style: poppinsMedium(size: 13.sp, color: gray)
           ),
-
-          SizedBox(height: 10.h),
-
-          // Custom Page Indicator
+          SizedBox(height: 6.h),
+          Text(
+            "Electric Bike",
+            style: poppinsSemiBold(size: 21.sp, color: black)
+          ),
+          SizedBox(height: 6.h),
+          Text(
+            "by Bhrati Electra",
+            style: poppinsMedium(size: 13.sp, color: gray)
+          ),
+          SizedBox(height: 14.h),
           Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: List.generate(imageUrls.length, (index) {
-              return Container(
-                width: 12.w,
-                height: 2.5.h,
-                margin: const EdgeInsets.symmetric(horizontal: 4),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10),
-                  color: activeIndex == index ? black.withOpacity(0.8) : black.withOpacity(0.1),
-                ),
-              );
-            }),
-          ),
-
-          SizedBox(height: 20.h),
-
-          // Product Details
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: 20.sp),
-            child: const Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  "Category 2",
-                  style: TextStyle(fontSize: 14, color: Colors.grey),
-                ),
-                Text(
-                  "Hooded Sweatshirt",
-                  style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
-                ),
-                Text(
-                  "by Jarvis Pepperspray",
-                  style: TextStyle(fontSize: 14, color: Colors.grey),
-                ),
-                SizedBox(height: 8),
-                Row(
+            children: [
+              RichText(
+                text: TextSpan(
+                  style: poppinsSemiBold(size: 10.sp, color: black),
                   children: [
-                    Text(
-                      "\$125.00",
-                      style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: primary),
+                    TextSpan(
+                      text: "2000 ",
+                      style: poppinsSemiBold(color: primary, size: 20.sp ),
                     ),
-                    SizedBox(width: 10),
-                    Text(
-                      "\$200.00",
-                      style: TextStyle(fontSize: 16, color: Colors.grey, decoration: TextDecoration.lineThrough),
+                    TextSpan(
+                      text: "CCT",
+                      style: poppinsSemiBold(color: black, size: 20.sp ),
                     ),
                   ],
                 ),
-                SizedBox(height: 12),
-                Text(
-                  "Description",
-                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(width: 12.w),
+              Text(
+                "/",
+                style: poppinsSemiBold(size: 18.sp, color: black)
+              ),
+              SizedBox(width: 12.w),
+              RichText(
+                text: TextSpan(
+                  style: poppinsSemiBold(size: 10.sp, color: black),
+                  children: [
+                    TextSpan(
+                      text: "120 ",
+                      style: poppinsSemiBold(color: primary, size: 20.sp ),
+                    ),
+                    TextSpan(
+                      text: "Watt",
+                      style: poppinsSemiBold(color: black, size: 20.sp ),
+                    ),
+                  ],
                 ),
-                SizedBox(height: 6),
-                Text(
-                  "Mauris neque tellus, placerat sit amet quam et, facilisis suscipit dui. "
-                  "Cras a pretium ena mauris. Mauris eget sapien ut nisi posuere.",
-                  style: TextStyle(fontSize: 14, color: Colors.grey),
-                ),
-              ],
-            ),
+              ),
+            ],
           ),
-
-          const Spacer(),
-
-          // Add to Cart Button
-          Container(
-            width: double.infinity,
-            margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
-            child: ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                backgroundColor: primary,
-                padding: const EdgeInsets.symmetric(vertical: 16),
-                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
-              ),
-              onPressed: () {},
-              child: const Text(
-                "Add To Cart",
-                style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold, color: black),
-              ),
-            ),
+          SizedBox(height: 14.h),
+          Text(
+            "Technical Specification",
+            style: poppinsSemiBold(size: 18.sp, color: black)
+          ),
+          SizedBox(height: 6.h),
+          Text(
+            "Mauris neque tellus, placerat sit amet quam et, facilisis suscipit dui. "
+            "Mauris neque tellus, placerat sit amet quam et, facilisis suscipit dui. "
+            "Mauris neque tellus, placerat sit amet quam et, facilisis suscipit dui. "
+            "Mauris neque tellus, placerat sit amet quam et, facilisis suscipit dui. "
+            "Mauris neque tellus, placerat sit amet quam et, facilisis suscipit dui. "
+            "Mauris neque tellus, placerat sit amet quam et, facilisis suscipit dui. "
+            "Mauris neque tellus, placerat sit amet quam et, facilisis suscipit dui. "
+            "Mauris neque tellus, placerat sit amet quam et, facilisis suscipit dui. "
+            "Mauris neque tellus, placerat sit amet quam et, facilisis suscipit dui. "
+            "Cras a pretium ena mauris. Mauris eget sapien ut nisi posuere.",
+            style: poppinsMedium(size: 13.sp, color: gray)
           ),
         ],
       ),
+    );
+  }
+
+
+  // Product Image slider
+  productImageSlider() {
+    return Column(
+      children: [
+        CarouselSlider(
+          items: imageUrls.map((url) {
+            return GestureDetector(
+              onTap: (){
+                Get.to(()=> FullScreenImagePage(imageUrl: url));
+              },
+              child: Container(
+                color: const Color.fromARGB(255, 236, 236, 236),
+                child: Image.network(
+                  url,
+                  fit: BoxFit.cover,
+                  width: double.infinity,
+                ),
+              ),
+            );
+          }).toList(),
+          options: CarouselOptions(
+            height: 300.h,
+            viewportFraction: 1.0,
+            autoPlay: true,
+            onPageChanged: (index, reason) {
+              setState(() {
+                activeIndex = index;
+              });
+            },
+          ),
+        ),
+        SizedBox(height: 8.h),
+        // Custom Page Indicator
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: List.generate(imageUrls.length, (index) {
+            return Container(
+              width: 12.w,
+              height: 2.5.h,
+              margin: const EdgeInsets.symmetric(horizontal: 4),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                color: activeIndex == index ? black.withOpacity(0.8) : black.withOpacity(0.1),
+              ),
+            );
+          }),
+        ),
+    
+      ],
     );
   }
 }
