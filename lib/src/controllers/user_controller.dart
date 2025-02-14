712f042717ec963f3bbe1c27e40e756230114f3b
rilonmaharjan <rilon.maharjan@gmail.com>
2025-02-14 14:25:28 +0545

    [modified user controller] - implemented update profile and bank detail api

--- lib/src/controllers/user_controller.dart
+++ lib/src/controllers/user_controller.dart
@@ -1,19 +1,65 @@
+import 'dart:developer';
+
 import 'package:get/get.dart';
+import 'package:pecon/src/api_config/api_repo.dart';
 import 'package:pecon/src/app_config/read_write.dart';
+import 'package:pecon/src/widgets/custom_toast.dart';
 
 class UserController extends GetxController {
   final RxBool isLoginLoading = false.obs;
+  final RxBool isProfileBtnLoading = false.obs;
+  final RxBool isBankBtnLoading = false.obs;
   int userPoints = read("user") != "" ? read("user")["earned_points"] ?? 0 : 0;
   String userImage = read("user") != ""? read("user")["profile_url"] ?? "https://images.squarespace-cdn.com/content/v1/56c346b607eaa09d9189a870/1551408857522-4ZFG11B2M7UPFYBFBRO0/FLAUNT-MAGAZINE-JOJI-2.jpg" : "https://images.squarespace-cdn.com/content/v1/56c346b607eaa09d9189a870/1551408857522-4ZFG11B2M7UPFYBFBRO0/FLAUNT-MAGAZINE-JOJI-2.jpg";
   String userName = read("user") != "" ? read("user")["name"] ?? "N/A" : "N/A";
   String userId =  read("user") != "" ? read("user")["id"].toString() != "null" ? read("user")["id"].toString() : "N/A" : "N/A";
   String userNumber = read("user") != "" ? read("user")["number"].toString() != "null" ? read("user")["number"].toString() : "N/A" : "N/A";
 
-  void startLoginLoading() {
-    isLoginLoading.value = true;
+  //update profile
+  updateProfile({name, number, email, address, gender, dob}) async{
+    var data = {
+      "name": name,
+      "number": number,
+      "email": email,
+      "address" : address,
+      "gender": gender,
+      "dob" : dob
+    };
+    try{
+      isProfileBtnLoading(true);// Start Loading
+      var response = await ApiRepo.apiPut('profile/update', data, 'Update Profile');
+      if(response != null && response['code'] == 201) {
+        Get.back();
+        showToast(isSuccess: true, message: "Profile Details Updated");
+      }
+    }catch (e){
+      log(e.toString());
+    } finally{
+      isProfileBtnLoading(false);
+    }
   }
 
-  void stopLoginLoading() {
-    isLoginLoading.value = false;
+  //update bank
+  updateBank({accName, accNum, bankName, branchName, esewaNum, khaltiNum}) async{
+    var data = {
+      "account_holder_name": accName,
+      "account_number": accNum,
+      "bank_name": bankName,
+      "branch" : branchName,
+      "esewa": esewaNum,
+      "khalti" : khaltiNum
+    };
+    try{
+      isBankBtnLoading(true);// Start Loading
+      var response = await ApiRepo.apiPut('profile/update-bank-details', data, 'Update Bank Details');
+      if(response != null && response['code'] == 201) {
+        Get.back();
+        showToast(isSuccess: true, message: "Bank Details Updated");
+      }
+    }catch (e){
+      log(e.toString());
+    } finally{
+      isBankBtnLoading(false);
+    }
   }
 }
--- lib/src/view/profile_form_page.dart
+++ lib/src/view/profile_form_page.dart
@@ -1,9 +1,12 @@
 import 'dart:io';
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:pecon/src/app_config/styles.dart';
+import 'package:pecon/src/controllers/user_controller.dart';
 import 'package:pecon/src/widgets/custom_appbar.dart';
 import 'package:pecon/src/widgets/custom_button.dart';
 import 'package:pecon/src/widgets/custom_network_image.dart';
@@ -17,19 +20,35 @@ class ProfileFormPage extends StatefulWidget {
 }
 
 class _ProfileFormPageState extends State<ProfileFormPage> {
+  //Get controller
+  final UserController userCon =Get.put(UserController());
+
+  //Form keys
   final formKey = GlobalKey<FormState>();
+  final bankFormKey = GlobalKey<FormState>();
 
   dynamic changedProfileImage;
 
+  //gender selection
+  List<String> gender = ["Male", "Female", "Others"];
+  int selectedIndex = 0; // Store initial selection
+
+  //initial DOB
+  DateTime selectedDate = DateTime.now(); 
 
   // Profile Text Editing Controllers 
   final TextEditingController nameController     = TextEditingController();
+  final TextEditingController emailController    = TextEditingController();
   final TextEditingController numController      = TextEditingController();
+  final TextEditingController addressController  = TextEditingController();
+  final TextEditingController genderController   = TextEditingController();
+  final TextEditingController dobController      = TextEditingController();
 
   // Bank Text Editing Controllers 
   final TextEditingController accNameController  = TextEditingController();
   final TextEditingController bankController     = TextEditingController();
   final TextEditingController accNoController    = TextEditingController();
+  final TextEditingController branchController   = TextEditingController();
   final TextEditingController esewaController    = TextEditingController();
   final TextEditingController khaltiController   = TextEditingController();
 
@@ -126,7 +145,7 @@ class _ProfileFormPageState extends State<ProfileFormPage> {
       child: Column(
         children: [
           changeProfilePic(),
-          SizedBox(height: 30.h,),
+          SizedBox(height: 20.h,),
           profileInfoForm(),
         ],
       )
@@ -145,13 +164,56 @@ class _ProfileFormPageState extends State<ProfileFormPage> {
             textInputAction: TextInputAction.next,
             headingText: "User Name",
             filledColor: gray.withOpacity(0.2),
+            validator: (value) => value != ""
+                ? null
+                : "Required",
           ),
           SizedBox(height: 20.h),
           //number
           CustomTextFormField(
             controller: numController,
             textInputAction: TextInputAction.next,
+            keyboardType: TextInputType.number,
             headingText: "Mobile Number",
+            filledColor: gray.withOpacity(0.2),
+            validator: (value) => value != ""
+                ? null
+                : "Required",
+          ),
+          SizedBox(height: 20.h),
+          //Email
+          CustomTextFormField(
+            controller: emailController,
+            textInputAction: TextInputAction.next,
+            headingText: "Email",
+            filledColor: gray.withOpacity(0.2),
+          ),
+          SizedBox(height: 20.h),
+          //Address
+          CustomTextFormField(
+            controller: addressController,
+            textInputAction: TextInputAction.next,
+            headingText: "Address",
+            filledColor: gray.withOpacity(0.2),
+          ),
+          SizedBox(height: 20.h),
+          //Gender
+          CustomTextFormField(
+            onTap: showCupertinoGenderPicker,
+            readOnly: true,
+            controller: genderController,
+            textInputAction: TextInputAction.next,
+            headingText: "Gender",
+            filledColor: gray.withOpacity(0.2),
+          ),
+          SizedBox(height: 20.h),
+          //Dob
+          CustomTextFormField(
+            onTap: showCupertinoDatePicker,
+            readOnly: true,
+            controller: dobController,
+            textInputAction: TextInputAction.done,
+            headingText: "Date of Birth",
             filledColor: gray.withOpacity(0.2),
           ),
         ],
@@ -174,7 +236,7 @@ class _ProfileFormPageState extends State<ProfileFormPage> {
   //bank form
   bankInfoForm() {
     return Form(
-      key: formKey,
+      key: bankFormKey,
       child: Column(
         children: [
           //Name
@@ -183,6 +245,9 @@ class _ProfileFormPageState extends State<ProfileFormPage> {
             textInputAction: TextInputAction.next,
             headingText: "Account Holder Name",
             filledColor: gray.withOpacity(0.2),
+            validator: (value) => value != ""
+                ? null
+                : "Required",
           ),
           SizedBox(height: 20.h),
           //bank name
@@ -191,6 +256,9 @@ class _ProfileFormPageState extends State<ProfileFormPage> {
             textInputAction: TextInputAction.next,
             headingText: "Bank Name",
             filledColor: gray.withOpacity(0.2),
+            validator: (value) => value != ""
+                ? null
+                : "Required",
           ),
           SizedBox(height: 20.h),
           //bank acc no
@@ -198,6 +266,17 @@ class _ProfileFormPageState extends State<ProfileFormPage> {
             controller: accNoController,
             textInputAction: TextInputAction.next,
             headingText: "Account Number",
+            filledColor: gray.withOpacity(0.2),
+            validator: (value) => value != ""
+                ? null
+                : "Required",
+          ),
+          SizedBox(height: 20.h),
+          //branchName
+          CustomTextFormField(
+            controller: branchController,
+            textInputAction: TextInputAction.next,
+            headingText: "Branch Name",
             filledColor: gray.withOpacity(0.2),
           ),
           SizedBox(height: 20.h),
@@ -208,21 +287,15 @@ class _ProfileFormPageState extends State<ProfileFormPage> {
             keyboardType: TextInputType.number,
             headingText: "Esewa Number",
             filledColor: gray.withOpacity(0.2),
-            validator: (value) => value != null && value.length == 10
-                ? null
-                : "Enter a valid 10-digit mobile number",
           ),
           SizedBox(height: 20.h),
           //khalti number
           CustomTextFormField(
             controller: khaltiController,
-            textInputAction: TextInputAction.next,
+            textInputAction: TextInputAction.done,
             keyboardType: TextInputType.number,
             headingText: "Khalti Number",
             filledColor: gray.withOpacity(0.2),
-            validator: (value) => value != null && value.length == 10
-                ? null
-                : "Enter a valid 10-digit mobile number",
           ),
         ],
       ),
@@ -374,23 +447,154 @@ class _ProfileFormPageState extends State<ProfileFormPage> {
   
   //submit button
   submitButton() {
-    return 
     // Submit Button
-    // Obx(()=>
+    return Obx(()=>
       Padding(
         padding: EdgeInsets.symmetric(horizontal: 10.0.w),
         child: Center(
           child: CustomButton(
             width: double.infinity,
-            // isLoading: authCon.isLoginLoading.value,
-            onPressed: () async {
-              final isValid = formKey.currentState!.validate();
-                if (!isValid) return;
-            },
+            isLoading: isProfileView ? userCon.isProfileBtnLoading.value : userCon.isBankBtnLoading.value,
+            onPressed: isProfileView 
+              ? () async {
+                final isValid = formKey.currentState!.validate();
+                  if (!isValid) return;
+                  await userCon.updateProfile(
+                    name: nameController.text.toString().trim(),
+                    number: numController.text.toString().trim(),
+                    email: emailController.text.toString().trim(),
+                    address: addressController.text.toString().trim(),
+                    gender: genderController.text.toString().trim(),
+                    dob: dobController.text.toString().trim(),
+                  );
+              }
+              : () async {
+                final isValid =  bankFormKey.currentState!.validate();
+                  if (!isValid) return;
+                  await userCon.updateBank(
+                    accName: accNameController.text.toString().trim(),
+                    bankName: bankController.text.toString().trim(),
+                    accNum: accNoController.text.toString().trim(),
+                    branchName: branchController.text.toString().trim(),
+                    esewaNum: esewaController.text.toString().trim(),
+                    khaltiNum: khaltiController.text.toString().trim()
+                  );
+              },
             text: "Submit",
           ),
         ),
-      );
-    // ),
+      ),
+    );
   }
+
+  //gender picker
+  showCupertinoGenderPicker() {
+    showCupertinoModalPopup(
+      context: context,
+      builder: (BuildContext context) {
+        return Container(
+          height: 240.h,
+          color: Colors.white,
+          child: Column(
+            children: [
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 16.sp),
+                height: 40.h,
+                color: Colors.grey[200],
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    TextButton(
+                      onPressed: () => Navigator.pop(context), // Cancel
+                      child: Text("Cancel", style: poppinsMedium(size:15.sp, color: purple),),
+                    ),
+                    TextButton(
+                      onPressed: () {
+                        setState(() {
+                          genderController.text = gender[selectedIndex]; // Update gender
+                        });
+                        Navigator.pop(context);
+                      },
+                      child: Text("Done", style: poppinsMedium(size:15.sp, color: purple),),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 200.h,
+                child: CupertinoPicker(
+                  backgroundColor: Colors.white,
+                  itemExtent: 40.h,
+                  scrollController: FixedExtentScrollController(
+                    initialItem: selectedIndex,
+                  ),
+                  onSelectedItemChanged: (index) {
+                    selectedIndex = index;
+                  },
+                  children: gender.map((role) => Center(child: Text(role, style: TextStyle(fontSize: 18.sp),))).toList(),
+                ),
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+
+  //DOB picker
+  showCupertinoDatePicker() {
+    showModalBottomSheet(
+      context: context,
+      builder: (context) {
+        return Container(
+          height: 280.h,
+          color: Colors.white,
+          child: Column(
+            children: [
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 16.sp),
+                height: 40.h,
+                color: Colors.grey[200],
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    TextButton(
+                      onPressed: () => Navigator.pop(context), // Cancel
+                      child: Text("Cancel", style: poppinsMedium(size:15.sp, color: purple),),
+                    ),
+                    TextButton(
+                      onPressed: () {
+                        setState(() {
+                          dobController.text = "${selectedDate.year}-${selectedDate.month}-${selectedDate.day}";
+                        });
+                        Navigator.pop(context);
+                      },
+                      child: Text("Done", style: poppinsMedium(size:15.sp, color: purple),),
+                    ),
+                  ],
+                ),
+              ),
+              // Date Picker
+              Expanded(
+                child: CupertinoDatePicker(
+                  initialDateTime: selectedDate, // Default selection
+                  mode: CupertinoDatePickerMode.date,
+                  minimumDate: DateTime(1925, 1, 1), // Start from 1950
+                  maximumDate: DateTime.now(),       // Until today
+                  onDateTimeChanged: (DateTime newDate) {
+                    if (newDate.isAfter(DateTime.now())) {
+                      setState(() => selectedDate = DateTime.now()); // Reset if beyond today
+                    } else {
+                      setState(() => selectedDate = newDate);
+                    }
+                  },
+                ),
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+
 }
