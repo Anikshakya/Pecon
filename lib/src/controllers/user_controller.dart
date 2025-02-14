2bee03375cade89181b6db26e15cde1e51f5e858
Anik Shakya <aniklinkin@gmail.com>
2025-02-14 11:11:59 +0545

    [managed] - null issue for id

--- lib/src/controllers/user_controller.dart
+++ lib/src/controllers/user_controller.dart
@@ -6,7 +6,7 @@ class UserController extends GetxController {
   int userPoints = read("user") != "" ? read("user")["earned_points"] ?? 0 : 0;
   String userImage = read("user") != ""? read("user")["profile_url"] ?? "https://images.squarespace-cdn.com/content/v1/56c346b607eaa09d9189a870/1551408857522-4ZFG11B2M7UPFYBFBRO0/FLAUNT-MAGAZINE-JOJI-2.jpg" : "https://images.squarespace-cdn.com/content/v1/56c346b607eaa09d9189a870/1551408857522-4ZFG11B2M7UPFYBFBRO0/FLAUNT-MAGAZINE-JOJI-2.jpg";
   String userName = read("user") != "" ? read("user")["name"] ?? "N/A" : "N/A";
-  String userId =  read("user") != "" ? read("user")["id"] ?? "N/A" : "N/A";
+  String userId =  read("user") != "" ? read("user")["id"].toString() != "null" ? read("user")["id"].toString() : "N/A" : "N/A";
   String userNumber = read("user") != "" ? read("user")["number"] ?? 0 : 0;
 
   void startLoginLoading() {
