6ac4e8d44747e8be6e5be80c9e7404aa05764bad
Anik Shakya <aniklinkin@gmail.com>
2025-03-04 11:25:06 +0545

    Fixed - Registration

--- lib/src/view/register_page.dart
+++ lib/src/view/register_page.dart
@@ -29,7 +29,7 @@ class _RegisterPageState extends State<RegisterPage> {
   bool isObscure = true;
   bool isConfirmPassObscure = true;
 
-  List<String> roles = ["Vendor", "Customer", "Technician"];
+  List<String> roles = ["Shopkeeper", "Customer", "Technician"];
   String selectedRole = "Customer";
 
   @override
