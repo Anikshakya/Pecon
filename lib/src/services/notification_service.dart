4a5531b267005813758eb4cd909e088edf1b5a1a
rilonmaharjan <rilon.maharjan@gmail.com>
2025-08-18 12:00:37 +0545

    [modifeid]

--- lib/src/services/notification_service.dart
+++ lib/src/services/notification_service.dart
@@ -2,11 +2,11 @@ import 'dart:async';
 import 'dart:convert';
 import 'dart:io';
 import 'dart:math';
-import 'dart:developer';
  
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+// ignore: depend_on_referenced_packages
 import 'package:http/http.dart' as http;
 import 'package:path_provider/path_provider.dart';
 // ignore: depend_on_referenced_packages
