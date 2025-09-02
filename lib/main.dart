69d485d588c4ee8ce027c3c0e3c29cd4aa65caaf
rilonmaharjan <rilon.maharjan@gmail.com>
2025-09-02 17:11:52 +0545

    [modified]

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:faded/faded.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:get_storage/get_storage.dart';
