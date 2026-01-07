89c4d8c22efd78b0d42b90067b5bdff2c92ad22c
anik <anik_mi@yonefu.info>
2026-01-07 18:13:04 +0545

    Disable screem shot

--- android/app/src/main/kotlin/com/pecon/np/pecon_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/pecon/np/pecon_app/MainActivity.kt
@@ -1,5 +1,18 @@
 package com.jujutech.np.pecon
 
 import io.flutter.embedding.android.FlutterActivity
+import android.view.WindowManager.LayoutParams
 
-class MainActivity : FlutterActivity()
+class MainActivity: FlutterActivity() {
+    override fun onResume() {
+        super.onResume()
+        // Disable screenshots and screen recording
+        window.addFlags(LayoutParams.FLAG_SECURE)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        // Re-enable screenshots and screen recording (optional)
+        window.clearFlags(LayoutParams.FLAG_SECURE)
+    }
+}
